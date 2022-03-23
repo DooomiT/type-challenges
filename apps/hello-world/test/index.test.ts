d6151ede2dd6e560bdba91d89ebf0da9a21e4e7b
DooomiT <Florian.Bufler@gmail.com>
2022-03-23 21:08:38 +0100

    Add HelloWorld challenge

---
+++ apps/hello-world/test/index.test.ts
@@ -0,0 +1,7 @@
+import { expect, it } from 'vitest';
+import { HelloWorld } from '../types/index';
+
+it('Type of HelloWorld should be string', () => {
+  const helloWorld: HelloWorld = 'Hello World';
+  expect(typeof helloWorld === 'string');
+});
