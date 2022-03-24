3facd7fd46705003795c915d5386dd03a42e0d5c
DooomiT <Florian.Bufler@gmail.com>
2022-03-24 17:36:51 +0100

    Add multiple challenge results

--- apps/easy/src/index.ts
+++ apps/easy/src/index.ts
@@ -3,7 +3,9 @@
  */
 type HelloWorld = string;
 
-const helloWorld: HelloWorld = "Hello World";
+const helloWorld = () => {
+  const helloWorld: HelloWorld = "Hello World";
+};
 
 /**
  * https://github.com/type-challenges/type-challenges/blob/master/questions/4-easy-pick/README.md
@@ -12,15 +14,52 @@ type MyPick<T, K extends keyof T> = {
   [key in K]: T[K];
 };
 
-interface Todo {
-  title: string;
-  description: string;
-  completed: boolean;
-}
+const myPick = () => {
+  interface Todo {
+    title: string;
+    description: string;
+    completed: boolean;
+  }
 
-type TodoPreview = MyPick<Todo, "title" | "completed">;
+  type TodoPreview = MyPick<Todo, "title" | "completed">;
 
-const todo: TodoPreview = {
-  title: "Clean room",
-  completed: false,
+  const todo: TodoPreview = {
+    title: "Clean room",
+    completed: false,
+  };
 };
+
+/**
+ * https://github.com/type-challenges/type-challenges/blob/master/questions/7-easy-readonly/README.md
+ */
+type MyReadonly<T> = {
+  readonly [key in keyof T]: T[key];
+};
+
+const readonly = () => {
+  interface Todo {
+    title: string;
+    description: string;
+  }
+
+  const todo: MyReadonly<Todo> = {
+    title: "Hey",
+    description: "foobar",
+  };
+
+  /** Throws error in tsc -> readonly
+   * todo.title = 'Hello';
+   * todo.description = 'barFoo';
+   */
+};
+
+/**
+ * https://github.com/type-challenges/type-challenges/blob/master/questions/14-easy-first/README.md
+ */
+type First<T extends any[]> = T extends [] ? never : T[0];
+
+type arr1 = ["a", "b", "c"];
+type arr2 = [3, 2, 1];
+
+type head1 = First<arr1>; // expected to be 'a'
+type head2 = First<arr2>; // expected to be 3
