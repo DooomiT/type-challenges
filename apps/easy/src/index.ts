28f0cb0084392e5bc74c7b7ab46cea1af271fab4
DooomiT <Florian.Bufler@gmail.com>
2022-03-24 17:23:16 +0100

    Add MyPick

--- apps/easy/src/index.ts
+++ apps/easy/src/index.ts
@@ -4,3 +4,23 @@
 type HelloWorld = string;
 
 const helloWorld: HelloWorld = "Hello World";
+
+/**
+ * https://github.com/type-challenges/type-challenges/blob/master/questions/4-easy-pick/README.md
+ */
+type MyPick<T, K extends keyof T> = {
+  [key in K]: T[K];
+};
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+type TodoPreview = MyPick<Todo, "title" | "completed">;
+
+const todo: TodoPreview = {
+  title: "Clean room",
+  completed: false,
+};
